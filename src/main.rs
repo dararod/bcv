c1a3157dcfbc0cad9e0215e572b9f7e23c6f49c1
dararod <dalejorod@gmail.com>
2024-05-30 21:33:15 -0400

    feat: implement `retrieves_price` function

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ const URL: &str = "https://www.bcv.org.ve/tasas-informativas-sistema-bancario";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let document = fetch_html().await?;
+    if let Some(currency) = retrieves_price(&document, "dolar")? {
+        println!("1 USD = {} Bs.s", currency);
+    }
 
+    if let Some(currency) = retrieves_price(&document, "euro")? {
+        println!("1 EUR = {} Bs.s", currency);
+    }
     Ok(())
 }
 
@@ -15,16 +21,21 @@ async fn fetch_html() -> Result<Html, Box<dyn std::error::Error>> {
     Ok(document)
 }
 
-fn retrieves_price(document: Html, currency: String) -> Result<Option<String>, Box<dyn std::error::Error>> {
+fn retrieves_price(
+    document: &Html,
+    currency: &str,
+) -> Result<Option<String>, Box<dyn std::error::Error>> {
     let currency_request = format!("div#{}", currency);
-    let dolar_selector = Selector::parse(&currency_request)?;
-    let results = document.select(&dolar_selector);
+    let Ok(currency_selector) = Selector::parse(&currency_request) else {
+        return Err("Invalid currency selector".into());
+    };
+    let results = document.select(&currency_selector);
 
     if let Some(dolar_block) = results.into_iter().next() {
         let strong_selector = Selector::parse("strong")?;
         let price_selector = dolar_block.select(&strong_selector);
         if let Some(el) = price_selector.into_iter().next() {
-            println!("{}", el.inner_html())
+            return Ok(Some(el.inner_html()));
         }
     }
     Ok(None)
