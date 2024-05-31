0078d50ee4c8b60516e0745c2da637aa9b081934
dararod <dalejorod@gmail.com>
2024-05-30 21:13:31 -0400

    feat: create `retrieves_price` function

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,20 @@ const URL: &str = "https://www.bcv.org.ve/tasas-informativas-sistema-bancario";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let document = fetch_html().await?;
-    let dolar_selector = Selector::parse("div#dolar")?;
+
+    Ok(())
+}
+
+async fn fetch_html() -> Result<Html, Box<dyn std::error::Error>> {
+    let html = reqwest::get(URL).await?.text().await?;
+
+    let document = Html::parse_document(&html);
+    Ok(document)
+}
+
+fn retrieves_price(document: Html, currency: String) -> Result<Option<String>, Box<dyn std::error::Error>> {
+    let currency_request = format!("div#{}", currency);
+    let dolar_selector = Selector::parse(&currency_request)?;
     let results = document.select(&dolar_selector);
 
     if let Some(dolar_block) = results.into_iter().next() {
@@ -14,12 +27,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("{}", el.inner_html())
         }
     }
-    Ok(())
-}
-
-async fn fetch_html() -> Result<Html, Box<dyn std::error::Error>> {
-    let html = reqwest::get(URL).await?.text().await?;
-
-    let document = Html::parse_document(&html);
-    Ok(document)
+    Ok(None)
 }
